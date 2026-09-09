7c53f088363b4450b54111e602ceb5e49bdf9eed
ConfusedGameDev <98442105+ConfusedGameDev@users.noreply.github.com>
2026-09-09 18:02:00 +0900

    Add: is-palindrome - submission-21

---
+++ Data Structures & Algorithms/is-palindrome/submission-21.cs
@@ -0,0 +1,63 @@
+public class Solution {
+    public bool IsPalindrome(string s) 
+    {
+        
+ 
+        s= s.ToUpper();
+        int revI= s.Length-1;
+        int i=0;
+        while(i<revI)
+        {
+            
+           // Console.WriteLine($"{i}, {revI}");
+            if( !char.IsLetterOrDigit(s[i]))
+            {
+                  i++;
+                  continue;
+               // Console.WriteLine($"A{i}, {revI}");
+
+            }
+          
+            while( !char.IsLetterOrDigit(s[revI]))
+            {
+                revI--;
+                continue;
+              //  Console.WriteLine($"B{i}, {revI}");
+            }
+            
+            
+            var next = s[i];
+
+            var next2= s[revI];
+
+            
+            if((next>=48 && next<58) || (next>=65 && next<91) )
+            {
+                //Console.WriteLine($"Comparing {s[i]},{s[revI]}");
+               if(s[i]!= s[revI])
+               return false;
+               
+               
+                
+            
+            }
+            else
+            {
+                continue;
+            }
+           
+           
+            revI--;
+            i++;
+
+
+        }
+
+     
+        
+        return true;
+        
+    }
+
+    
+}
