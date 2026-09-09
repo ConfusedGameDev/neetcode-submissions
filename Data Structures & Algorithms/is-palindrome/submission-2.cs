38c68846d441860c9dca24ec1c20f5004827c897
ConfusedGameDev <98442105+ConfusedGameDev@users.noreply.github.com>
2026-09-09 17:27:14 +0900

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cs
@@ -0,0 +1,40 @@
+public class Solution {
+    public bool IsPalindrome(string s) 
+    {
+        Queue<char> leftToRight= new ();
+        Stack<char> rightToLeft= new ();
+
+ 
+        s= s.ToUpper();
+        for(int i=0; i< s.Length;i++)
+        {
+            var next = s[i];
+            if((next>=48 && next<58) || (next>=65 && next<91)  ||(next>=97 && next<123) )
+            {
+                //Console.WriteLine($"Adding {next}");
+             leftToRight.Enqueue(next);
+            rightToLeft.Push(next);
+            }
+            else
+            {
+                // Console.WriteLine($"ignoring {next}");
+            }
+           
+
+
+        }
+
+     
+       while(leftToRight.Count>0)
+        {
+            var x= leftToRight.Dequeue();
+            var y = rightToLeft.Pop();
+
+           // Console.WriteLine($"Comparing {x}, {y}");
+            if(x!=y)
+            return false;
+        }
+        return true;
+        
+    }
+}
