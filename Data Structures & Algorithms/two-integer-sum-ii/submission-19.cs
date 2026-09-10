f40712cd283e337072680d236aef5434d18bc83e
ConfusedGameDev <98442105+ConfusedGameDev@users.noreply.github.com>
2026-09-10 19:04:45 +0900

    Add: two-integer-sum-ii - submission-19

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-19.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target) 
+    {
+        
+        int x= 0;
+        int y=numbers.Length-1;
+
+        while(x<y)
+        {
+            
+            if(numbers[x]+numbers[y]>target)
+            {
+                y--;
+                continue;
+            }
+            else  if(numbers[x]+numbers[y]<target)
+            {
+                x++;
+                continue;
+            }
+
+            else
+            {
+                return new int[]{x+1,y+1};
+            }
+
+        }
+        
+        return new int[]{};
+    }
+
+  
+
+}
